c6815b0bd3d70326d8f318e8d20aa1d50481c983
BufnastaZecica <regovicsanja@gmail.com>
2021-09-30 13:58:11 +0100

    almost there

--- js/index.js
+++ js/index.js
@@ -1,51 +1,13 @@
-// const { application } = require("express");
 
-// const e = require("express");
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
 
-// const hamburger = document.querySelector(".hamburger");
-// const navLinks = document.querySelector(".nav-links");
-// const links = document.querySelectorAll(".nav-links li");
-
-// hamburger.addEventListener("click", () => {
-//   navLinks.classList.toggle("open");
-//   links.forEach(link => {
-//     link.classList.toggle("fade");
-//   });
-// });
-
-
-
-const contactForm = document.querySelector('#contact-form');
-let name = document.getElementById('user_name');
-let email = document.getElementById('user_email');
-let message = document.getElementById('user_message');
-
-
-contactForm.addEventListener('submit', (e)=>{
-    e.preventDefault();
-
-  let formData = {
-    name: user_name.value,
-    email: user_email.value,
-    message: user_message.value
-
-  };
-
-
-  let xhr = new XMLHttpRequest();
-  xhr.open('POST', '/');
-  xhr.setRequestHeader('content-type', 'application/json');
-  xhr.onload = function() {
-    console.log(xhr.responseText);
-    if(xhr.responseText == 'success') {
-      alert('Email sent!');
-      user_name.value = '';
-      user_email.value = '';
-      user_message.value = '';
-    }else{
-      alert('Something went wrong!');
-    }
-  }
-  xhr.send(JSON.stringify(formData));
-
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+  links.forEach(link => {
+    link.classList.toggle("fade");
+  });
 });
+
+
